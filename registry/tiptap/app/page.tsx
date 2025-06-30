49a30a9432f0d8c4058467ffe654c7ad7196c102
joeBlockchain <94577719+joeBlockchain@users.noreply.github.com>
2025-06-29 23:39:23 -0700

    Add Tiptap editor component with customizable toolbar and code block support; update documentation and registry

---
+++ registry/tiptap/app/page.tsx
@@ -0,0 +1,9 @@
+import Tiptap from "../components/tiptap";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+        <Tiptap />
+    </div>
+  );
+} 
